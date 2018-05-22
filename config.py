dca82dd7274200797364190c17204914c082b86a
Matthew Munson <matthew.munson@uni-hamburg.de>
2018-05-22 10:30:00 +0200

    Transformed flask_nemo to a dependency
    Added login and login_required

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    SECRET_KEY = os.environ.get('NEMO_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
---
+++ formulae/app.py
@@ -0,0 +1,39 @@
+from flask import Flask
+from capitains_nautilus.cts.resolver import NautilusCTSResolver
+from capitains_nautilus.flask_ext import FlaskNautilus
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_migrate import Migrate
+
+
+flask_app = Flask("Flask Application for Nemo")
+flask_app.config.from_object(Config)
+db = SQLAlchemy(flask_app)
+login = LoginManager(flask_app)
+login.login_view = '.r_login'
+migrate = Migrate(flask_app, db)
+resolver = NautilusCTSResolver(["/home/matt/results/CapiTainS_input"])
+resolver.parse()
+
+from formulae import models
+
+nautilus_api = FlaskNautilus(prefix="/api", resolver=resolver, app=flask_app)
+
+from .nemo import NemoFormulae
+
+nemo = NemoFormulae(
+    name="InstanceNemo",
+    app=flask_app,
+    resolver=resolver,
+    base_url="",
+    css=["assets/css/theme.css"],
+    js=["assets/js/empty.js"],
+    static_folder="./assets/",
+    transform={"default": "components/epidoc.xsl",
+               "notes": "components/extract_notes.xsl"},
+    templates={"main": "templates/main"}
+)
+
+
+
