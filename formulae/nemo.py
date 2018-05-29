662d436a2939418c4e8b7e94d97c7d95cb74fa29
Matthew Munson <matthew.munson@uni-hamburg.de>
2018-05-29 13:08:00 +0200

    Removed the r_pdf route and made all text views run through r_multipassage

--- formulae/nemo.py
+++ formulae/nemo.py
@@ -19,18 +19,15 @@ class NemoFormulae(Nemo):
         ("/collections", "r_collections", ["GET"]),
         ("/collections/<objectId>", "r_collection", ["GET"]),
         ("/text/<objectId>/references", "r_references", ["GET"]),
-        ("/text/<objectId>/passage/<subreference>", "r_passage", ["GET"]),
         ("/texts/<objectIds>/passage/<subreferences>", "r_multipassage", ["GET"]),
-        ("/text/<objectId>/passage", "r_first_passage", ["GET"]),
         ("/login", "r_login", ["GET", "POST"]),
         ("/logout", "r_logout", ["GET"]),
         ("/user/<username>", "r_user", ["GET", "POST"]),
-        ("/pdfs/<objectIds>", "r_pdfs", ["GET"]),
         ("/add_text/<objectIds>/<reffs>", "r_add_text_collections", ["GET"]),
         ("/add_text/<objectId>/<objectIds>/<reffs>", "r_add_text_collection", ["GET"])
     ]
     SEMANTIC_ROUTES = [
-        "r_collection", "r_references", "r_passage", "r_multipassage"
+        "r_collection", "r_references", "r_multipassage"
     ]
 
     FILTERS = [
@@ -46,7 +43,7 @@ class NemoFormulae(Nemo):
 
     CACHED = [
         # Routes
-        "r_index", "r_collection", "r_collections", "r_references", "r_passage", "r_first_passage", "r_assets", "r_multipassage",
+        "r_index", "r_collection", "r_collections", "r_references", "r_assets", "r_multipassage",
         # Controllers
         "get_inventory", "get_collection", "get_reffs", "get_passage", "get_siblings",
         # Translater
@@ -56,9 +53,10 @@ class NemoFormulae(Nemo):
     ]
 
     PROTECTED = [
-        "r_index", "r_collections", "r_collection", "r_references", "r_passage", "r_multipassage", "r_first_passage",
-        "r_register"
+        "r_index", "r_collections", "r_collection", "r_references", "r_multipassage", "r_register"
     ]
+
+    OPEN_COLLECTIONS = ["Formulae Mondseer"]
 
     def __init__(self, *args, **kwargs):
         if "pdf_folder" in kwargs:
@@ -86,15 +84,6 @@ class NemoFormulae(Nemo):
         :return: a string of the values joined by the separator
         """
         return s.join(l)
-
-    def f_make_members(self, collection, lang=None):
-        """ Turn the make_members function into a filter
-
-        :param collection: Collection to build dict view of for its members
-        :param lang: Language to express data in
-        :return: List of basic objects
-        """
-        return self.make_members(collection, lang)
 
     def view_maker(self, name, instance=None):
         """ Create a view
@@ -169,6 +158,7 @@ class NemoFormulae(Nemo):
         :return: Template, collections metadata and Markup object representing the text
         :rtype: {str: Any}
         """
+        pdf_path = ''
         collection = self.get_collection(objectId)
         if isinstance(collection, CtsWorkMetadata):
             editions = [t for t in collection.children.values() if isinstance(t, CtsEditionMetadata)]
@@ -182,6 +172,8 @@ class NemoFormulae(Nemo):
         else:
             notes = ''
         prev, next = self.get_siblings(objectId, subreference, text)
+        if current_user.project_team is False and str(text.get_creator(lang)) not in self.OPEN_COLLECTIONS:
+            pdf_path = self.pdf_folder + objectId.split(':')[-1] + '.pdf'
         return {
             "template": "main::text.html",
             "objectId": objectId,
@@ -203,7 +195,8 @@ class NemoFormulae(Nemo):
             "text_passage": Markup(passage),
             "notes": Markup(notes),
             "prev": prev,
-            "next": next
+            "next": next,
+            "pdf_path": pdf_path
         }
 
     def r_multipassage(self, objectIds, subreferences, lang=None):
@@ -224,44 +217,6 @@ class NemoFormulae(Nemo):
         for i, id in enumerate(ids):
             d = self.r_passage(id, subrefers[i], lang=lang)
             del d['template']
-            passage_data['objects'].append(d)
-        return passage_data
-
-    def r_pdfs(self, objectIds, lang=None):
-        """ Return the pdf(s) of the request texts
-
-        :param objectIds:
-        :return: Template, collections and object metadata, path to pdf file
-        :rtype: {str: Any}
-        """
-        ids = objectIds.split('+')
-        passage_data = {'template': 'main::pdfs.html', 'objects': []}
-        for id in ids:
-            collection, reffs = self.get_reffs(objectId=id, export_collection=True)
-            first, _ = reffs[0]
-            if isinstance(collection, CtsWorkMetadata):
-                editions = [t for t in collection.children.values() if isinstance(t, CtsEditionMetadata)]
-                if len(editions) == 0:
-                    raise UnknownCollection("This work has no default edition")
-                id = str(editions[0].id)
-            text = self.get_passage(objectId=id, subreference=first)
-            d = {"objectId": id,
-                "collections": {
-                    "current": {
-                        "label": collection.get_label(lang),
-                        "id": collection.id,
-                        "model": str(collection.model),
-                        "type": str(collection.type),
-                        "author": text.get_creator(lang),
-                        "title": text.get_title(lang),
-                        "description": text.get_description(lang),
-                        "citation": collection.citation,
-                        "coins": self.make_coins(collection, text, first, lang=lang)
-                    },
-                    "parents": self.make_parents(collection, lang=lang)
-                },
-                "pdf_path": self.pdf_folder + id.split(':')[-1] + '.pdf'
-            }
             passage_data['objects'].append(d)
         return passage_data
 
