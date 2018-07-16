93fa414d279016fa77eaa0acb6757d77e91e8461
Matthew Munson <matthew.munson@uni-hamburg.de>
2018-07-16 14:25:48 +0200

    Attempts to register a custom 404 error handler
    This is not working right now because there is no url rule for a random file request (e.g., /file.pdf) which I am using to test the 404 error. The question is whether I need a custom 404 error handler if the error needs to come for a registered URL. If it does, perhaps I can always return a more informational error than 404

--- formulae/nemo.py
+++ formulae/nemo.py
@@ -72,7 +72,7 @@ class NemoFormulae(Nemo):
         self.app.jinja_env.filters["remove_from_list"] = self.f_remove_from_list
         self.app.jinja_env.filters["join_list_values"] = self.f_join_list_values
         self.app.jinja_env.filters["replace_indexed_item"] = self.f_replace_indexed_item
-        # self.app.register_error_handler(404, self.e_not_found_error)
+        self.app.register_error_handler(404, self.e_not_found_error)
         # self.app.register_error_handler(500, self.e_internal_error)
         self.app.before_request(self.before_request)
 
@@ -83,6 +83,8 @@ class NemoFormulae(Nemo):
         """
         blueprint = super(NemoFormulae, self).create_blueprint()
         blueprint.register_error_handler(UnknownCollection, self.e_unknown_collection_error)
+        # blueprint.register_error_handler(500, self.e_internal_error)
+        # blueprint.register_error_handler(404, self.e_not_found_error)
         return blueprint
 
     def f_remove_from_list(self, l, i):
@@ -437,6 +439,7 @@ class NemoFormulae(Nemo):
 
     def e_unknown_collection_error(self, error):
         response = error.args[0].strip("\"'").split()[0]
+        print(request.endpoint)
         return self.r_display_error(error_code="UnknownCollection", error_message=response)
 
     def r_display_error(self, error_code, error_message):
@@ -450,6 +453,6 @@ class NemoFormulae(Nemo):
             return self.render(**{"template": 'main::unknown_collection.html', 'message': error_message,
                     'parent': '.'.join(error_message.split('.')[:-1]), 'url': dict()})
         if error_code == "500":
-            return self.render(**{"template": 'main::500.html', 'url': dict()})
+            return self.render(**{"template": 'main::500.html', 'url': {'error': '500'}}), 500
         if error_code == "404":
-            return self.render(**{"template": 'main::404.html', 'url': dict()})
+            return self.render(**{"template": 'main::404.html', 'message': error_message, 'url': dict()})
