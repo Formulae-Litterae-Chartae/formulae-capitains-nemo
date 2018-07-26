fd2426dc6e8da5840d632d8a90dcb4ddc26c0f31
Matthew Munson <matthew.munson@uni-hamburg.de>
2018-07-26 12:53:11 +0200

    Changed the ADMINS config variable (#4)
    * Changed the ADMINS config variable

--- config.py
+++ config.py
@@ -18,4 +18,4 @@ class Config(object):
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['your-email@example.com']
+    ADMINS = os.environ.get('ADMINS').split(';') if os.environ.get('ADMINS') else ['no-reply@example.com']
