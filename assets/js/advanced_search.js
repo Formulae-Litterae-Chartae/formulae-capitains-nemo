88760bba7c83cf35c08f6b6adcd3ce89d442f872
Matthew Munson <matthew.munson@uni-hamburg.de>
2019-02-15 13:46:24 +0100

    Issued At autocomplete results now must start with the letters in the search box

--- assets/js/advanced_search.js
+++ assets/js/advanced_search.js
@@ -133,3 +133,13 @@ function updateParam(par) {
         params[par] = this.value;
     }
 }
+
+// autocomplete for the Issued At search using JQuery UI
+$( "#place-search" ).autocomplete({
+    source: function( request, response ) {
+        var matcher = new RegExp( "^" + $.ui.autocomplete.escapeRegex( request.term ), "i" );
+        response( $.grep( tags, function( item ){
+            return matcher.test( item );
+        }) );
+    }
+});
