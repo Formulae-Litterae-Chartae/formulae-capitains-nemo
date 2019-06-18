a84c801582b7af4213cbaa7b9bd7120cf37d7ef8
Matthew Munson <matthew.munson@uni-hamburg.de>
2019-06-18 11:10:20 +0200

    Added make_coins as function

--- formulae/nemo.py
+++ formulae/nemo.py
@@ -248,6 +248,33 @@ class NemoFormulae(Nemo):
             route = login_required(route)
         return route
 
+    def make_coins(self, collection, text, subreference="", lang=None):
+        """ Creates a CoINS Title string from information
+
+        :param collection: Collection to create coins from
+        :param text: Text/Passage object
+        :param subreference: Subreference
+        :param lang: Locale information
+        :return: Coins HTML title value
+        """
+        if lang is None:
+            lang = self.__default_lang__
+        return "url_ver=Z39.88-2004"\
+                 "&ctx_ver=Z39.88-2004"\
+                 "&rft_val_fmt=info%3Aofi%2Ffmt%3Akev%3Amtx%3Abook"\
+                 "&rft_id={cid}"\
+                 "&rft.genre=bookitem"\
+                 "&rft.btitle={title}"\
+                 "&rft.edition={edition}"\
+                 "&rft.au={author}"\
+                 "&rft.atitle={pages}"\
+                 "&rft.language={language}"\
+                 "&rft.pages={pages}".format(
+                    title=quote(str(text.get_title(lang))), author=quote(str(text.get_creator(lang))),
+                    cid=url_for("InstanceNemo.r_collection", objectId=collection.id, _external=True),
+                    language=collection.lang, pages=quote(subreference), edition=quote(str(text.get_description(lang)))
+                 )
+
     def r_collection(self, objectId, lang=None):
         data = super(NemoFormulae, self).r_collection(objectId, lang=lang)
         if self.check_project_team() is False:
