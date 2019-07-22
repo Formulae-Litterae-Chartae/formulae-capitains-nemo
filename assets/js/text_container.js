d436b2f129fc9fd29f31fe2891189fa9ab47681f
Gargamel19 <trendelenburger19.04@googlemail.com>
2019-07-22 14:45:39 +0200

    addet the sidebar collapse toggle buttons

---
+++ assets/js/text_container.js
@@ -0,0 +1,26 @@
+    var collapse_r = false
+
+    $(document).ready(function () {
+        $('#sidebarCollapse_l').on('click', function () {
+            $('#sidebar_l').toggleClass('active');
+            var el_l = document.getElementById('sidebarCollapse_l');
+            if ($('#sidebar_l').css("marginLeft")=='0px'){
+                el_l.firstChild.data = "<";
+            }else{
+                el_l.firstChild.data = ">";
+            }
+        });
+    });
+
+    $(document).ready(function () {
+        $('#sidebarCollapse_r').on('click', function () {
+            $('#sidebar_r').toggleClass('active');
+            var el_r = document.getElementById('sidebarCollapse_l');
+
+            if ($('#sidebar_r').active){
+                el_r.firstChild.data = "<";
+            }else{
+                el_r.firstChild.data = ">";
+            }
+        });
+    });
