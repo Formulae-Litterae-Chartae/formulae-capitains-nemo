94fa590111bf36b56db6dfbbab31344405e258fe
Gargamel19 <trendelenburger19.04@googlemail.com>
2019-07-25 12:34:17 +0200

    ich weiß noch nicht, warum aber scheinbar benötigt das js die JQUERY Referenz. und die Button beschriftung ändeert sich noch immer noch nicht.

--- assets/js/text_container.js
+++ assets/js/text_container.js
@@ -1,4 +1,23 @@
+    var collapsed_r = false;
+
     $(document).ready(function () {
+        $('#sidebarCollapse_r').on('click', function () {
+            $('#sidebar_r').toggleClass('active');
+            console.lod("HAI");
+            var el_r = document.getElementById('sidebarCollapse_r');
+
+            if (collapsed_r){
+                collapsed_r = false;
+                console.lod(collapsed_r);
+                el_r.firstChild.data = ">";
+            }else{
+                collapsed_r = true;
+                console.lod(collapsed_r);
+                el_r.firstChild.data = "<";
+            }
+        });
+
+
         $('#sidebarCollapse_l').on('click', function () {
             $('#sidebar_l').toggleClass('active');
             var el_l = document.getElementById('sidebarCollapse_l');
@@ -8,17 +27,7 @@
                 el_l.firstChild.data = ">";
             }
         });
+
     });
 
-    $(document).ready(function () {
-        $('#sidebarCollapse_r').on('click', function () {
-            $('#sidebar_r').toggleClass('active');
-            var el_r = document.getElementById('sidebarCollapse_r');
 
-            if (el_r.firstChild.data = "<"){
-                el_r.firstChild.data = ">";
-            }else{
-                el_r.firstChild.data = "<";
-            }
-        });
-    });
