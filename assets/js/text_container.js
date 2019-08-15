4dd439b92110c0a6409f30868461c410ded02640
Gargamel19 <trendelenburger19.04@googlemail.com>
2019-08-15 11:13:57 +0200

    fixed the jQuery and the Button text problem

--- assets/js/text_container.js
+++ assets/js/text_container.js
@@ -1,33 +1,23 @@
-    var collapsed_r = false;
 
-    $(document).ready(function () {
-        $('#sidebarCollapse_r').on('click', function () {
-            $('#sidebar_r').toggleClass('active');
-            console.lod("HAI");
-            var el_r = document.getElementById('sidebarCollapse_r');
+    $(document).ready(function() {
 
-            if (collapsed_r){
-                collapsed_r = false;
-                console.lod(collapsed_r);
-                el_r.firstChild.data = ">";
-            }else{
-                collapsed_r = true;
-                console.lod(collapsed_r);
-                el_r.firstChild.data = "<";
-            }
-        });
-
-
-        $('#sidebarCollapse_l').on('click', function () {
+        $('#sidebarCollapse_l').click(function() {
             $('#sidebar_l').toggleClass('active');
             var el_l = document.getElementById('sidebarCollapse_l');
-            if ($('#sidebar_l').css("marginLeft")=='0px'){
+            if ($('#sidebar_l').css('marginLeft')=='0px'){
                 el_l.firstChild.data = "<";
             }else{
                 el_l.firstChild.data = ">";
             }
         });
 
+        $('#sidebarCollapse_r').click(function() {
+            $('#sidebar_r').toggleClass('active');
+            var el_r = document.getElementById('sidebarCollapse_r');
+            if (el_r.firstChild.data == "<"){
+                el_r.firstChild.data = ">";
+            }else{
+                el_r.firstChild.data = "<";
+            }
+        });
     });
-
-
