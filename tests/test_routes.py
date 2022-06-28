1f2ee91ee5c7b64ad947a8d52c326a50dbf01f82
Matthew Munson <matthew.munson@uni-hamburg.de>
2022-06-28 15:10:34 +0200

    Changed one test for assertEqual to assertCountEqual

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -646,7 +646,7 @@ class TestIndividualRoutes(Formulae_Testing):
                              [('urn:cts:formulae:marculf.form000.lat001', 'Marculf I Prolog (lat)')],
                              'Linked editions should be correctly passed to the template.')
             c.get('/corpus/urn:cts:formulae:flavigny', follow_redirects=True)
-            self.assertEqual(self.get_context_variable('collections')['current']['containing_collections'],
+            self.assertCountEqual(self.get_context_variable('collections')['current']['containing_collections'],
                              [(Markup('Ko<span class="manuscript-number">2</span>'),
                                rdflib.term.Literal('Kopenhagen, Kongelige Bibliotek, Fabr. 84', lang='deu'),
                                'urn:cts:formulae:ko2'),
