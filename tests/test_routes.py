67d0eee4e3e035afe3057b93d0e2874798553e63
Matt Munson <matthew.munson@uni-hamburg.de>
2023-11-09 11:04:38 +0100

    Put Franziska's data behind a 401 code for non-project users

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -504,12 +504,12 @@ class TestIndividualRoutes(Formulae_Testing):
             self.assertEqual(r.request.path, '/auth/login')
             c.get('/videos', follow_redirects=True)
             self.assertIn('main::videos.html', [x[0].name for x in self.templates])
-            c.get('/charter_parts', follow_redirects=True)
-            self.assertIn('main::all_parts.html', [x[0].name for x in self.templates])
-            c.get('/charter_groups', follow_redirects=True)
-            self.assertIn('main::charter_groups.html', [x[0].name for x in self.templates])
-            c.get('/charter_formulaic', follow_redirects=True)
-            self.assertIn('main::charter_formulae.html', [x[0].name for x in self.templates])
+            response = c.get('/charter_parts', follow_redirects=True)
+            self.assertEqual(response.status_code, 401, 'Should raise 401 error.')
+            response = c.get('/charter_groups', follow_redirects=True)
+            self.assertEqual(response.status_code, 401, 'Should raise 401 error.')
+            response = c.get('/charter_formulaic', follow_redirects=True)
+            self.assertEqual(response.status_code, 401, 'Should raise 401 error.')
 
     def test_authorized_project_member(self):
 
@@ -795,6 +795,12 @@ class TestIndividualRoutes(Formulae_Testing):
             r = c.get('/auth/remove_page/{}'.format(page_to_remove.id), follow_redirects=True)
             self.assertEqual(r.request.path, '/auth/saved_pages')
             self.assertEqual([], [p for p in current_user.pages])
+            c.get('/charter_parts', follow_redirects=True)
+            self.assertIn('main::all_parts.html', [x[0].name for x in self.templates])
+            c.get('/charter_groups', follow_redirects=True)
+            self.assertIn('main::charter_groups.html', [x[0].name for x in self.templates])
+            c.get('/charter_formulaic', follow_redirects=True)
+            self.assertIn('main::charter_formulae.html', [x[0].name for x in self.templates])
 
     def test_authorized_normal_user(self):
         """ Make sure that all routes are open to normal users but that some texts are not available"""
