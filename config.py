eb29f212bf04c3d36cf8a810e28d8144975b36bc
tschomacker <thorben.schomacker@gmail.com>
2026-05-28 14:32:49 +0200

    #1101 config update

--- config.py
+++ config.py
@@ -15,6 +15,7 @@ class Config(object):
     ES_CLIENT_KEY = os.environ.get('ES_CLIENT_KEY', '')
     ES_API_KEY = os.environ.get('ES_API_KEY', '')
     ES_VERIFY_CERTS = os.environ.get('ES_VERIFY_CERTS', 'false')
+    ES_EXCLUDE_COLLECTIONS = os.environ.get('ES_EXCLUDE_COLLECTIONS').split(';') if os.environ.get('ES_EXCLUDE_COLLECTIONS') else False
 
     LANGUAGES = ['en', 'de', 'fr']
     BABEL_DEFAULT_LOCALE = 'de'
